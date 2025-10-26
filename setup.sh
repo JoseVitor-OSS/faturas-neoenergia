0014a7a147580d3e67f13cc4b1ce8ad3c0749978
José Vitor Oliveira de Souza Santos <155102949+JoseVitor-OSS@users.noreply.github.com>
2025-10-26 11:02:42 -0300

    Add files

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Chrome
+apt-get update
+apt-get install -y wget
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Install ChromeDriver
+CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
+wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
+mv chromedriver /usr/local/bin/
+
+# Clean up
+rm chromedriver_linux64.zip
